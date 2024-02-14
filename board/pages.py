ba69f3bab8c8fbc8ffba077a1ed7a0b0321b61ba
wilwarin-n <michiru.aware.lrn@gmail.com>
2024-02-14 23:58:25 +0300

    Create about.html

--- board/pages.py
+++ board/pages.py
@@ -1,11 +1,11 @@
-from flask import Blueprint
+from flask import Blueprint, render_template
 
 bp = Blueprint("pages", __name__)
 
 @bp.route("/")
 def home():
-    return "Hello, Home!"
+    return render_template("home.html")
 
 @bp.route("/about")
 def about():
-    return "Hello, About!"
+    return render_template("about.html")

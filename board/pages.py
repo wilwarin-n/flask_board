683e99a9899acfb9512fd0b9439a62cdeb0e1dfe
wilwarin-n <michiru.aware.lrn@gmail.com>
2024-02-14 17:38:15 +0300

    add pages.py - main template

--- board/pages.py
+++ board/pages.py
@@ -1,11 +1,11 @@
 from flask import Blueprint
 
-bp = Blueprint("pages",__name__)
+bp = Blueprint("pages", __name__)
 
-bp.route("/")
+@bp.route("/")
 def home():
     return "Hello, Home!"
 
-bp.route("/about")
+@bp.route("/about")
 def about():
     return "Hello, About!"

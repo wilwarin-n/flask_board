a9d4f27b5ab90e43b2d2aa9c007ac442badb2231
wilwarin-n <michiru.aware.lrn@gmail.com>
2024-02-15 09:12:06 +0300

    change return_template files

--- board/pages.py
+++ board/pages.py
@@ -4,8 +4,8 @@ bp = Blueprint("pages", __name__)
 
 @bp.route("/")
 def home():
-    return render_template("home.html")
+    return render_template("pages/home.html")
 
 @bp.route("/about")
 def about():
-    return render_template("about.html")
+    return render_template("pages/about.html")

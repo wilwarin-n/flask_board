95f1b9a305a42a799add10a7ff16d1d23fa44a98
wilwarin-n <michiru.aware.lrn@gmail.com>
2024-02-19 00:37:07 +0300

    Update posts.py and posts.html

--- board/posts.py
+++ board/posts.py
@@ -1,12 +1,30 @@
-from flask import Blueprint, render_template
+from flask import (Blueprint, render_template,redirect,request,url_for,)
+
+from board.database import get_db
 
 bp = Blueprint("posts",__name__)
 
 @bp.route("/create", methods=("GET","POST"))
 def create():
-    return render_template("post/create.html")
+    if request.method == 'POST':
+        author = request.form["author"] or "Anonymus"
+        message = request.form["message"]
+
+        if message:
+            db=get_db()
+            db.execute(
+                "INSERT INTO post (author, message) VALUES (?,?)",
+                (author,message),
+            )
+            db.commit()
+            return redirect(url_for("posts.posts"))
+        
+    return render_template("posts/create.html")
 
 @bp.route("/posts")
 def posts():
-    posts = []
-    return render_template("posts/posts.html",posts = posts)
+    db=get_db()
+    posts = db.execute(
+        "SELECT author, message, created FROM post ORDER BY created DESC"
+    ).fetchall()
+    return render_template("posts/posts.html", posts=posts)
